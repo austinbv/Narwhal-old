937a5b50f7b0b772ac0d4d775edb2c3d5f847109
Austin <austinbv@gmail.com>
2011-12-04 18:00:22 -0500

    Recent Presentations not include any user presentations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,11 @@
 class UsersController < ApplicationController
   expose(:users)
   expose(:user) { User.where(:id => params[:id]).includes([:presentations, :created_presentations]).first}
-  expose(:recent_presentations) { Presentation.where("updated_at > ?", 8.days.ago).limit(10).order("updated_at DESC").all }
   expose(:participated_presentations) do
     user.presentations - user.created_presentations
+  end
+  expose(:recent_presentations) do
+    Presentation.where("updated_at > ?", 8.days.ago).limit(10).order("updated_at DESC").all - participated_presentations
   end
 
   def index
